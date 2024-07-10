ee17b20c7d3b8c4b44305ee01dd1d2601c05e581
ALFREDO MALDONADO <alfredorafael.maldonado@alumno.iesluisvives.org>
2024-07-10 17:03:04 +0200

    finish

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  imageSource:any;
+  constructor(private DomSanitizer:DomSanitizer) {}
+  takePicture = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source:CameraSource.Prompt,
+      saveToGallery:true
+    });
+  //  this.imageSource='data:image/jpeg;base64,' + image.base64String;
 
-  constructor() {}
-
+   // console.log(this.imageSource)
+   this.imageSource=this.DomSanitizer.bypassSecurityTrustUrl(image.webPath ? image.webPath : "")
+}
+getPhoto(){
+  return this.imageSource
+}
 }
